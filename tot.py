034054d7f584b76bb3d5ac987d93988681fe4c3a
6u6lik16 <serega22222223@mail.ru>
2024-09-05 20:02:59 +0400

    Add files via upload

---
+++ tot.py
@@ -0,0 +1,6 @@
+Home_Work_Count = 12
+Hours_Count = 1.5
+Name_Kurs = "Python"
+Time_To_One_Task = (Hours_Count / Home_Work_Count)
+print(Time_To_One_Task)
+print("Курс:",Name_Kurs,", всего задач:", Home_Work_Count,", затрачено часов:", Hours_Count, ", cреднее время выполнения:", Time_To_One_Task)
